dbc1c93cfa190d137b6e7bacf83e20609ffbdcbd
marcos <marcos.adamczuk@gmail.com>
2020-10-08 17:45:57 -0300

    updated MessengerTest

--- src/main/java/dcb/core/utils/Pair.java
+++ src/main/java/dcb/core/utils/Pair.java
@@ -1,5 +1,7 @@
 package dcb.core.utils;
 
+import java.util.Objects;
+
 public class Pair<First ,Second> {
     public final First first;
     public final Second second;
@@ -7,6 +9,28 @@ public class Pair<First ,Second> {
     public Pair(First first, Second second) {
         this.first = first;
         this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
     }
 }
 
--- src/test/java/dcb/core/messaging/MessengerTest.java
+++ src/test/java/dcb/core/messaging/MessengerTest.java
@@ -1,16 +1,82 @@
 package dcb.core.messaging;
 
+import dcb.core.exceptions.DcbException;
+import dcb.core.models.Message;
+import dcb.core.models.NetworkAddress;
+import dcb.core.utils.BlockingQueueSender;
+import dcb.core.utils.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+@ExtendWith(MockitoExtension.class)
 class MessengerTest {
+    static final int SELF_ID = 10;
+    static final int REMOTE_ID = 1;
+    static final int LOCAL_ID = 2;
+    static final NetworkAddress REMOTE_ADDRESS = new NetworkAddress("localhost", 8080);
+    private static final Map<Integer, NetworkAddress> addresses;
+    private static final UUID uuid = UUID.randomUUID();
+
+    static {
+        addresses = new HashMap<>();
+        addresses.put(REMOTE_ID, REMOTE_ADDRESS);
+    }
+
+    @Mock
+    private BlockingQueueSender<Pair<Message, NetworkAddress>> networkSenderMock;
+
+    @Mock
+    private BlockingQueueSender<Message> localSenderMock;
+
+    private Messenger messenger = null;
+
+    private static Message messageFor(int target) {
+        return new Message(0L, 0L, SELF_ID, target, "", "", uuid, false);
+    }
+
+    @BeforeEach
+    void setUp() {
+        var localSenders = new HashMap<Integer, BlockingQueueSender<Message>>();
+        localSenders.put(LOCAL_ID, localSenderMock);
+        messenger = new Messenger(
+                localSenders,
+                addresses,
+                networkSenderMock
+        );
+    }
 
     @Test
-    void send() {
+    void send() throws DcbException, InterruptedException {
+        var localMsg = messageFor(LOCAL_ID);
+        var remoteMsg = messageFor(REMOTE_ID);
+
+        messenger.send(localMsg);
+        Mockito.verify(localSenderMock).put(localMsg);
+
+        messenger.send(remoteMsg);
+        Mockito.verify(networkSenderMock).put(new Pair<>(remoteMsg, REMOTE_ADDRESS));
     }
 
     @Test
     void sendLocally() {
+        var localMsg = messageFor(LOCAL_ID);
+        var remoteMsg = messageFor(REMOTE_ID);
+
+        try {
+            messenger.send(localMsg);
+        } catch (Exception ignored) {
+            assert false;
+        }
+
+        Assertions.assertThrows(DcbException.class, () -> messenger.sendLocally(remoteMsg));
     }
 }
