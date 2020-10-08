16c250c013cecf01b0c36436a7591ddbd3971b4a
marcos <marcos.adamczuk@gmail.com>
2020-10-08 19:29:36 -0300

    updated DependencyVectorTest

--- src/main/java/dcb/core/synchronization/DependencyVector.java
+++ src/main/java/dcb/core/synchronization/DependencyVector.java
@@ -4,14 +4,18 @@ import dcb.core.exceptions.DcbException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
+@SuppressWarnings("AccessingNonPublicFieldOfAnotherObject")
 public class DependencyVector {
-    private final int ownerComponentId;
-    private final Map<Integer, Long> componentIdToTimestamp;
+    private final int ownerId;
 
-    public DependencyVector(int componentId, Map<Integer, Long> componentIdToTimestamp) {
-        this.ownerComponentId = componentId;
-        this.componentIdToTimestamp = componentIdToTimestamp;
+    // Keys: component IDs, Values: Timestamps
+    private final Map<Integer, Long> timestamps;
+
+    public DependencyVector(int ownerId, Map<Integer, Long> timestamps) {
+        this.ownerId = ownerId;
+        this.timestamps = timestamps;
     }
 
     public static DependencyVector createNew(int componentId) {
@@ -20,40 +24,54 @@ public class DependencyVector {
 
     public DependencyVector copy() {
         Map<Integer, Long> mapClone = new HashMap<>();
-        for (Map.Entry<Integer, Long> entry : componentIdToTimestamp.entrySet()) {
+        for (Map.Entry<Integer, Long> entry : timestamps.entrySet()) {
             mapClone.put(entry.getKey(), entry.getValue());
         }
-        return new DependencyVector(ownerComponentId, mapClone);
+        return new DependencyVector(ownerId, mapClone);
     }
 
     public long get(int componentId) {
-        return componentIdToTimestamp.getOrDefault(componentId, 0L);
+        return timestamps.getOrDefault(componentId, 0L);
     }
 
-    public DependencyVector updateOwnerTimestamp(long timestamp) throws Exception {
-        if (get(ownerComponentId) > timestamp) {
-            throw new Exception();
+    public DependencyVector updateOwnerTimestamp(long timestamp) throws DcbException {
+        if (get(ownerId) > timestamp) {
+            throw new DcbException();
         }
         DependencyVector newDependencyVector = copy();
-        newDependencyVector.componentIdToTimestamp.put(ownerComponentId, timestamp);
+        newDependencyVector.timestamps.put(ownerId, timestamp);
         return newDependencyVector;
     }
 
-    public DependencyVector update(Map<Integer, Long> arrivingDependencyVector) throws DcbException {
+    public DependencyVector update(Map<Integer, Long> newVec) throws DcbException {
         DependencyVector newDependencyVector = copy();
-        for (Map.Entry<Integer, Long> entry : arrivingDependencyVector.entrySet()) {
+        for (Map.Entry<Integer, Long> entry : newVec.entrySet()) {
             int id = entry.getKey();
             long timestamp = entry.getValue();
 
-            if (id == ownerComponentId && timestamp > get(ownerComponentId)) {
+            if (id == ownerId && timestamp > get(ownerId)) {
                 throw new DcbException();
             }
 
-            if (id != ownerComponentId) {
+            if (id != ownerId) {
                 long correctTimestamp = Math.max(timestamp, newDependencyVector.get(id));
-                newDependencyVector.componentIdToTimestamp.put(id, correctTimestamp);
+                newDependencyVector.timestamps.put(id, correctTimestamp);
             }
         }
         return newDependencyVector;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DependencyVector that = (DependencyVector) o;
+        return ownerId == that.ownerId &&
+                Objects.equals(timestamps, that.timestamps);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ownerId, timestamps);
     }
 }
--- src/test/java/dcb/core/synchronization/DependencyVectorTest.java
+++ src/test/java/dcb/core/synchronization/DependencyVectorTest.java
@@ -1,20 +1,41 @@
 package dcb.core.synchronization;
 
+import dcb.core.exceptions.DcbException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
 
 class DependencyVectorTest {
+    static final int ID = 0;
 
     @Test
-    void copy() {
+    void updateOwnerTimestamp() throws Exception {
+        var vec = DependencyVector.createNew(ID);
+        Assertions.assertEquals(0L, vec.get(ID));
+        var vec2 = vec.updateOwnerTimestamp(10L);
+        Assertions.assertEquals(10L, vec2.get(ID));
+        Assertions.assertThrows(DcbException.class, () -> vec2.updateOwnerTimestamp(5L));
     }
 
     @Test
-    void updateOwnerTimestamp() {
-    }
+    void update() throws DcbException {
+        var firstMap = new HashMap<Integer, Long>();
+        firstMap.put(1, 15L);
+        var secondMap = new HashMap<Integer, Long>();
+        secondMap.put(1, 10L);
+        secondMap.put(2, 20L);
+        var thirdMap = new HashMap<Integer, Long>();
+        thirdMap.put(ID, 30L);
 
-    @Test
-    void update() {
+        var vec = DependencyVector.createNew(ID);
+        vec = vec.update(firstMap);
+        Assertions.assertEquals(15L, vec.get(1));
+        vec = vec.update(secondMap);
+        Assertions.assertEquals(15L, vec.get(1));
+        Assertions.assertEquals(20L, vec.get(2));
+
+        var finalVec = vec;
+        Assertions.assertThrows(DcbException.class, () -> finalVec.update(thirdMap));
     }
 }
